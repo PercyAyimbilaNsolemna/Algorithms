416ce20377531d1e6c3df165bdff1cfb3d1cef83
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-07-25 14:33:09 +0000

    Add comments to the selection sort.

--- Sorting/Selection sort/selectionSort.py
+++ Sorting/Selection sort/selectionSort.py
@@ -3,7 +3,7 @@
     
     The time complexity of selection sort is Big O(n^2) that is quadratic
     
-    The space complexity of selection sort is Big O()
+    The space complexity of selection sort is Big O(n) that is linear time 
     
 '''
 
@@ -17,7 +17,9 @@ def selectionSort(array):
     for i in range(0, array_length):
         #Comparing the ith element with all elements greater than the ith element 
         for j in range(i+1, array_length):
+            #Set the first element as minimum
             smallest_number = array[i]
+            #Update the minimum if current element is less than previous element
             if smallest_number > array[j]:
                 array[i], array[j] = array[j], array[i]
     return f'The sorted array {array}'
