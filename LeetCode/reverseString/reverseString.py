095c0e01027a019a31d30f3c862aa2d320d84153
PercyAyimbilaNsolemna <pnayimbila@st.ug.edu.gh>
2023-08-18 23:47:23 +0000

    Solve the leetcode reverse string.

---
+++ LeetCode/reverseString/reverseString.py
@@ -0,0 +1,28 @@
+'''
+    Write a function that reverses a string 
+    
+    Space complexity should be constant time 
+    
+'''
+
+class String:
+    def __str__(self):
+        return 'This is the string class'
+    
+    def reverse(self, word):
+        i = 0
+        j = len(word) - 1
+        
+        while i < j:
+            word[i], word[j] = word[j], word[i]
+            i += 1
+            j -= 1
+        return word
+    
+def main():
+    string = String()
+    print(string.reverse(['h', 'e', 'l', 'l', 'o']))
+    
+if __name__ == '__main__':
+    main()
+    
